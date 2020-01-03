d4ec0e92f6624094fe9150351fe68786d083c995
Will Martin <will@storyscript.io>
2020-01-03 15:26:50 +0100

    Extract functions for different items

--- gildedrose.go
+++ gildedrose.go
@@ -8,6 +8,20 @@ type Item struct {
 
 func UpdateInventory(items []*Item) {
 	for _, item := range items {
+		if item.name == "Anything" {
+			UpdateAnything(item)
+			continue
+		} else if item.name == "Aged Brie" {
+			UpdateBrie(item)
+			continue
+		} else if item.name == "Sulfuras, Hand of Ragnaros" {
+			UpdateSulfuras(item)
+			continue
+		} else if item.name == "Backstage passes to a TAFKAL80ETC concert" {
+			UpdateBackstage(item)
+			continue
+		}
+
 		if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" {
 			if item.quality > 0 {
 				if item.name != "Sulfuras, Hand of Ragnaros" {
@@ -54,5 +68,53 @@ func UpdateInventory(items []*Item) {
 			}
 		}
 	}
+}
 
+func UpdateAnything(item *Item) {
+	item.sellIn = item.sellIn - 1
+	if item.quality > 0 {
+		item.quality = item.quality - 1
+	}
+
+	if item.quality > 0 && item.sellIn < 0 {
+		item.quality = item.quality - 1
+	}
+}
+
+func UpdateBrie(item *Item) {
+	item.sellIn = item.sellIn - 1
+	if item.quality == 50 {
+		return
+	}
+
+	item.quality = item.quality + 1
+
+	if item.sellIn < 0 {
+		item.quality = item.quality + 1
+	}
+}
+
+func UpdateSulfuras(item *Item) {
+
+}
+
+func UpdateBackstage(item *Item) {
+	item.sellIn = item.sellIn - 1
+	item.quality = item.quality + 1
+
+	if item.sellIn < 10 {
+		item.quality = item.quality + 1
+	}
+
+	if item.sellIn < 5 {
+		item.quality = item.quality + 1
+	}
+
+	if item.sellIn < 0 {
+		item.quality = 0
+	}
+
+	if item.quality > 50 {
+		item.quality = 50
+	}
 }
