3bb3f3422be85f69435f15515b53fdf8d62ae53a
Will Martin <will@storyscript.io>
2020-01-10 16:42:05 +0100

    Clean up inventory repetition

--- gildedrose.go
+++ gildedrose.go
@@ -14,49 +14,50 @@ type GildedRose struct {
 
 func (g *GildedRose) UpdateInventory() {
 	for i := 0; i < len(g.Inventory); i++ {
+		item := g.Inventory[i]
 
-		if g.Inventory[i].Name != "Aged Brie" && g.Inventory[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if g.Inventory[i].Quality > 0 {
-				if g.Inventory[i].Name != "Sulfuras, Hand of Ragnaros" {
-					g.Inventory[i].Quality = g.Inventory[i].Quality - 1
+		if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+			if item.Quality > 0 {
+				if item.Name != "Sulfuras, Hand of Ragnaros" {
+					item.Quality = item.Quality - 1
 				}
 			}
 		} else {
-			if g.Inventory[i].Quality < 50 {
-				g.Inventory[i].Quality = g.Inventory[i].Quality + 1
-				if g.Inventory[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if g.Inventory[i].SellIn < 11 {
-						if g.Inventory[i].Quality < 50 {
-							g.Inventory[i].Quality = g.Inventory[i].Quality + 1
+			if item.Quality < 50 {
+				item.Quality = item.Quality + 1
+				if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+					if item.SellIn < 11 {
+						if item.Quality < 50 {
+							item.Quality = item.Quality + 1
 						}
 					}
-					if g.Inventory[i].SellIn < 6 {
-						if g.Inventory[i].Quality < 50 {
-							g.Inventory[i].Quality = g.Inventory[i].Quality + 1
+					if item.SellIn < 6 {
+						if item.Quality < 50 {
+							item.Quality = item.Quality + 1
 						}
 					}
 				}
 			}
 		}
 
-		if g.Inventory[i].Name != "Sulfuras, Hand of Ragnaros" {
-			g.Inventory[i].SellIn = g.Inventory[i].SellIn - 1
+		if item.Name != "Sulfuras, Hand of Ragnaros" {
+			item.SellIn = item.SellIn - 1
 		}
 
-		if g.Inventory[i].SellIn < 0 {
-			if g.Inventory[i].Name != "Aged Brie" {
-				if g.Inventory[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if g.Inventory[i].Quality > 0 {
-						if g.Inventory[i].Name != "Sulfuras, Hand of Ragnaros" {
-							g.Inventory[i].Quality = g.Inventory[i].Quality - 1
+		if item.SellIn < 0 {
+			if item.Name != "Aged Brie" {
+				if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
+					if item.Quality > 0 {
+						if item.Name != "Sulfuras, Hand of Ragnaros" {
+							item.Quality = item.Quality - 1
 						}
 					}
 				} else {
-					g.Inventory[i].Quality = g.Inventory[i].Quality - g.Inventory[i].Quality
+					item.Quality = item.Quality - item.Quality
 				}
 			} else {
-				if g.Inventory[i].Quality < 50 {
-					g.Inventory[i].Quality = g.Inventory[i].Quality + 1
+				if item.Quality < 50 {
+					item.Quality = item.Quality + 1
 				}
 			}
 		}
